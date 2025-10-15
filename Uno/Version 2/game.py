a7187fadfe97a2e1e4fcc2d7ec048015b23f0617
Sam018024 <018024@imberhorne.co.uk>
2025-10-15 10:56:18 +0100

    Add files via upload

--- Uno/Version 2/game.py
+++ Uno/Version 2/game.py
@@ -10,13 +10,68 @@ fgActiveHexa = "#F8D512"
 darkHexa = "#58585a"
 darkActiveHexa = "#515152"
 ##-----------------
+def setNext(root, enterPlayerEntry):
+    global Next
+    Next = True
+    global playerUserList
+    playerUserList.append(enterPlayerEntry.get)
+    root.destroy()
+##-----------------
 def lobby(root, playersQLabel, playerAmountCombobox, nextButton):
     print("lobby")
-    playerNum = playerAmountCombobox.get()
-    playersQLabel.forget()
-    playerAmountCombobox.forget()
-    nextButton.forget()
+    playerNum = int(playerAmountCombobox.get())
+    root.destroy()
+    global playerUserList
+    playerUserList = []
+    for i in range(0, playerNum):
+        global Next
+        Next = False
+        while Next != True:
+            
+            root = Tk()
+            root.geometry("300x120")
+            root.title("Player " + str(i+1))
+            root.configure(bg=bgHexa)
+            ##-----------------
+            enterPlayerLabel_var = StringVar()
+            enterPlayerLabel_var.set("Player " + str(i + 1) + "'s username:")
+            
+            enterPlayerLabel = Label(root,
+                                     textvariable = enterPlayerLabel_var,
+                                     anchor = CENTER,
+                                     bg = bgHexa,
+                                     fg = fgHexa,
+                                     font = ("Arial", 14, "bold")
+                                     )
+            ##-----------------
+            enterPlayerEntry_var = StringVar()
+            enterPlayerEntry_var.set("Username...")
+            
+            enterPlayerEntry = Entry(root,
+                                     textvariable = enterPlayerEntry_var,
+                                     font = ("Arial", 14),
+                                     bd = 0
+                                     )
+            ##-----------------
+            nextButton = Button(root,
+                        text = "Next",
+                        command = lambda: setNext(root, enterPlayerEntry),
+                        activebackground = fgActiveHexa,
+                        activeforeground = darkActiveHexa,
+                        anchor = "center",
+                        bg = fgHexa,
+                        fg = darkHexa,
+                        height = 4,
+                        width = 10,
+                        border = 0,
+                        font = ("Arial", 14, "bold")
+                        )             
+            enterPlayerLabel.pack(pady=(10,0))
+            enterPlayerEntry.pack()
+            nextButton.pack(pady=10)
+            root.mainloop()
     root.attributes("-fullscreen", True)
+    
 ##-----------------
 def playerChoice(root):
     root.geometry("300x400")
